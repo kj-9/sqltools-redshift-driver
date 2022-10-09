f14db55ae618c75ba602de2ab6ceab350bc65478
kj-9 <46800811+kj-9@users.noreply.github.com>
2022-10-09 21:36:04 +0900

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+  }
+}
