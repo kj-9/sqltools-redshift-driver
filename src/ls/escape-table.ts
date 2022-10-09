b277120fba43846fa15b05894233a21427468011
kj-9 <46800811+kj-9@users.noreply.github.com>
2022-10-09 23:33:12 +0900

    refactor escape table

--- src/ls/escape-table.ts
+++ src/ls/escape-table.ts
@@ -1,14 +1,14 @@
 import { NSDatabase } from "@sqltools/types";
 
-export const pgCheckEscape = (w: string | { label: string }) =>
-  /[^a-z0-9_]/.test((<any>w).label || w)
-    ? `"${(<any>w).label || w}"`
-    : (<any>w).label || w;
+const pgCheckEscape = (w: string) => {
+  return /[^a-z0-9_]/.test(w) ? `"${w}"` : w;
+};
 
 function escapeTableName(table: Partial<NSDatabase.ITable> | string) {
   const items: string[] = [];
   const tableObj =
     typeof table === "string" ? <NSDatabase.ITable>{ label: table } : table;
+
   tableObj.database && items.push(pgCheckEscape(tableObj.database));
   tableObj.schema && items.push(pgCheckEscape(tableObj.schema));
   items.push(pgCheckEscape(tableObj.label));
