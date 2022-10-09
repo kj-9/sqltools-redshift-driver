eeb9289b55d07b1f9b69616def601cf42ae0be09
kj-9 <46800811+kj-9@users.noreply.github.com>
2022-10-09 11:40:06 +0900

    add sample test suite

---
+++ test/suite/extention.test.ts
@@ -0,0 +1,30 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import PostgreSQL from '../../src/ls/driver';
+
+const conn = new PostgreSQL({
+    id: '',
+    isActive: true,
+    isConnected: true,
+    name: '',
+    driver: '',
+    username: '',
+    server: '',
+    database: '',
+    port: 1234,
+    password: ''
+}, async () => null)
+
+console.log(conn)
+
+suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+
+    test('Sample test', () => {
+        assert.strictEqual([1, 2, 3].indexOf(5), -1);
+        assert.strictEqual([1, 2, 3].indexOf(0), -1);
+    });
+});
