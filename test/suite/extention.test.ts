165e7059adede06d338b3518656d12409cc39f66
kj-9 <46800811+kj-9@users.noreply.github.com>
2022-10-10 15:17:51 +0900

    add connection test

--- test/suite/extention.test.ts
+++ test/suite/extention.test.ts
@@ -1,13 +1,16 @@
 import * as assert from "assert";
-import escapeTableName from "../../src/ls/escape-table";
+import * as vscode from "vscode";
 
-suite("Extension Test Suite", () => {
-  test("escape table name", () => {
+import escapeTableName from "../../src/ls/escape-table";
+import RedshiftDriver from "../../src/ls/driver";
+
+suite("Parser Test Suite", () => {
+  test("Escape table name only", () => {
     assert.strictEqual(escapeTableName("table"), "table");
     assert.strictEqual(escapeTableName("+table"), '"+table"');
   });
 
-  test("escape tablname with db and schema", () => {
+  test("Escape tablname with db and schema", () => {
     assert.strictEqual(
       escapeTableName({
         label: "table",
@@ -26,3 +29,39 @@ suite("Extension Test Suite", () => {
     );
   });
 });
+
+if (process.env.CIRCLECI) {
+  console.log("Running connection tests from CircleCI...");
+  suite("Connection Test Suite", () => {
+    const option = {
+      id: "test",
+      isConnected: false,
+      isActive: false,
+      pgOptions: {
+        ssl: true,
+      },
+      previewLimit: 50,
+      server: process.env.REDSHIFT_ENDPOINT,
+      port: parseInt(process.env.REDSHIFT_PORT),
+      driver: "Redshift Driver (Dedicated ver)",
+      name: "dev",
+      database: process.env.REDSHIFT_DATABASE,
+      username: process.env.REDSHIFT_USER,
+      password: process.env.REDSHIFT_PASSSWORD,
+    };
+    const driver = new RedshiftDriver(option, async () => {
+      const out = [];
+
+      for (const f of vscode.workspace.workspaceFolders) {
+        out.push(f);
+      }
+
+      return out;
+    });
+
+    test("Run a simple query", async () => {
+      const res = await driver.query("select 1 as test", {});
+      assert.deepStrictEqual(res[0].results, [{ test: 1 }]);
+    });
+  });
+}
